efde829542bb925cf067aa6f5393864bfb36439f
Sara Nolan <saraa.ann28@gmail.com>
2020-12-27 15:01:31 -0500

    added else & if statements and password requirements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
-  // GIVEN I need a new, secure password
-  // need a button or link to get a new password
-  // need to show the password on the page
+// GIVEN I need a new, secure password
+// need a button or link to get a new password
+// need to show the password on the page
 
 var generateBtn = document.querySelector("#generate");
 
@@ -20,53 +20,75 @@ function writePassword() {
 
 // ************************ function here *********************************
 
+//user input variables//
+// User input variables: 
+var enter;
+var confirmNumber;
+var confirmSymbols;
+var confirmUppercase;
+var confirmLowercase;
+
 //Password criteria//
-var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/"];
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var alphaLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // Add event listener to generate button//
 generateBtn.addEventListener("click", writePassword);
 
-// write a function called generatePassword which will have a series of prompts for user input//
+// start function to generate password//
 function generatePassword() {
-  let newPassword = "";
-  let passwordCharset = "";
 
-  // WHEN prompted for the length of the password, THEN I choose a length of at least 8 characters and no more than 128 characters 
-  let passwordLengthInput = prompt("How many characters would you like your password to be?");
-  if (Number(passwordLengthInput < 8) | Number(passwordLengthInput > 128) | isNaN(passwordLengthInput))
-    alert("Please enter a number between 8 and 128!");
-    return;}
+  // Asks for user input
+  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
 
-  // WHEN prompted for password criteria//
-  // THEN I select which criteria to include in the password
-  // WHEN prompted for the length of the password
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
-  // We have to ask the user for how many characters they want in a password
-  // we have to check to make sure it is between 8 and 128
-  // otherwise, we tell user to fix their input.
+  // WHEN prompted for the length of the password, THEN choose a length of at least 8 characters and no more than 128 characters 
+  // if no value is entered//
+  if (!enter) {
+    alert("Please enter a value");
 
-  // WHEN prompted for character types to include in the password
-  // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  // confirm if they want lowercase
-  // confirm if they want uppercase
-  // confirm if they want numeric
-  // confirm if they want special characters
+    //if a value does not meet criteria//
+  } else if (enter < 8 || enter > 128) {
+    enter = parseInt(prompt("You must choose a number between 8 and 128"));
 
-  // WHEN I answer each prompt
-  // THEN my input should be validated and at least one character type should be selected
-  // we need to show an error if they gave us no characters to choose from
+    //once length of password is validated//
+  } else {
+    // confirm if they want numeric
+    confirmNumber = confirm("Will this contain numbers?");
 
-  // WHEN all prompts are answered
-  // THEN a password is generated that matches the selected criteria
+    // confirm if they want characters
+    confirmSymbols = confirm("Will this contain special characters?");
+
+    // confirm if they want uppercase
+    confirmUppercase = confirm("Will this contain Uppercase letters?");
+
+    // confirm if they want lowercase
+    confirmLowercase = confirm("Will this contain Lowercase letters?");
+  };
+
+  // Else if for 4 negative options
+  if (!confirmSymbols && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+    choices = alert("You must choose a criteria!");
+
+  }
+
+
+}
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// we need to show an error if they gave us no characters to choose from
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
 // from all the characters chosen, choose one randomly and add it to our password X number of times
 // 
 
 // ************************ function ends here *********************************
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{}
+generateBtn.addEventListener("click", writePassword); { }
 
   // ************************ function ends here ********************************//
